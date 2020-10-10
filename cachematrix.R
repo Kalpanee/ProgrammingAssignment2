ea147158225e2c4b8559e9caf8fd5b2aa8ea2b66
Kalpanee <kalpaneeweer@gmail.com>
2020-10-10 22:34:20 +0530

    commited

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## set the input matrix as x
+#solved value "p" as NULL
+#this function creates special matrix object
+#that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  p<-NULLset<-function(y)
+  {
+    x<<-y
+    p<<-NULL
+  }
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function
+#it computes the inverse of the special matrix
+#if the inverse has been already calculted () and the matrix hasn`t changed)
+#then the cachesolve should retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+p<-x$getinverse()
+if(!is.null(p)){
+  message("getting inversed matrix")
+  return(p)
+}
+data <- x$grt()
+p<-solve(data, ...)
+x$setinverse(p)
+p
+}
+}
 }
